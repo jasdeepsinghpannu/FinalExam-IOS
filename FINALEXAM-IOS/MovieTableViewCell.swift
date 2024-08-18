a4372a6176a1e6b0f1151d6c4785aa75b8feb556
jasdeepsingh <200540018@student.georgianc.on.ca>
2024-08-17 21:33:22 -0400

    First commit

---
+++ FINALEXAM-IOS/MovieTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieTableViewCell.swift
+//  ICE7-IOS
+//
+//  Created by Jasdeep Singh on 2024-07-07.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell
+{
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var studioLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+}
---
+++ FINALEXAM-IOSUITests/ICE8_IOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ICE7_IOSUITestsLaunchTests.swift
+//  ICE7-IOSUITests
+//
+//  Created by Jasdeep Singh on 2024-07-07.
+//
+
+import XCTest
+
+final class ICE7_IOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
